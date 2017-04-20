794f41032b1b04dd7a0758b75732afd0761a1397
Jieun Rim <rim.jieun@gmail.com>
2017-04-20 17:42:02 -0400

    completed bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,46 +12,23 @@ connection.query("SELECT * FROM products", function(err, res) {
   if (err) throw err;
   displayProducts(res);
   console.log("==============================================");
-  runPrompt(res);
-  connection.end();
+  runCustomerPrompt(res);
+
 });
-
-function runPrompt(arr) {
-  inquirer.prompt([
-    {
-      
-    },
-    {
-      name: "item_units",
-      type: "input",
-      message: "How many units of this product would you like to buy?"
-    },
-    {
-      name: "confirm",
-      type: "confirm",
-      message: "Are you sure?",
-      default: true
-    }
-  ]).then(function(user) {
-    
-
-  });
-}
-
 
 
 function displayProducts(arr) {
   arr.forEach(function(item) {
     console.log("==============================================");
-    console.log("Product ID: " + item.item_id);
+    console.log("Item ID: " + item.item_id);
     console.log("Name: " + item.product_name);
-    console.log("Price: " + item.price);
+    console.log("Price: $" + item.price.toFixed(2));
     console.log("In stock: " + item.stock_quantity + " units");
     console.log("Department: " + item.department_name);
-  })
+  });
 }
 
-function promptProductID(arr) {
+function runCustomerPrompt(arr) {
   inquirer.prompt([
     {
       name: "item_id",
@@ -60,8 +37,61 @@ function promptProductID(arr) {
     }
   ]).then(function(user) {
     if (user.item_id > arr.length) {
-      console.log("That Product ID does not exist!");
-      promptProductID();
+      console.log("That Item ID does not exist!");
+    }
+    else {
+      var item = arr[user.item_id - 1];
+      var itemName = item.product_name;
+      inquirer.prompt([
+        {
+          name: "item_units",
+          type: "input",
+          message: "How many units of " + itemName + " would you like to buy?"
+        }
+      ]).then(function(user) {
+        if (user.item_units > item.stock_quantity) {
+          console.log("Insufficient quantity!");
+        }
+        else {
+          var orderedUnits = user.item_units;
+          inquirer.prompt([
+            {
+              name: "confirm",
+              type: "confirm",
+              message: "Are you sure you want to order " + orderedUnits + " units of " + itemName + "?",
+              default: true
+            }
+          ]).then(function(user) {
+            if (user.confirm) {
+              updateStock(item, orderedUnits);
+              // resetStock(item, 50);
+            }
+            else {
+              console.log("We're sorry you couldn't find what you needed. We hope you visit us again.");
+              connection.end();
+            }
+          });
+        }
+      });
     }
   });
 }
+
+function updateStock(obj, num) {
+  var itemID = obj.item_id;
+  var totalCost = obj.price * num;
+  var updatedUnits = obj.stock_quantity - num;
+  connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: updatedUnits}, {item_id: itemID}], function(err, res) {
+    if (err) throw err;
+    console.log("Thank you for your purchase. Your total was $" + totalCost.toFixed(2) + ".");
+    connection.end();
+  });
+}
+
+// function resetStock(obj, num) {
+//   var itemID = obj.item_id;
+//   connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: num}, {item_id: itemID}], function(err, res) {
+//     if (err) throw err;
+//     connection.end();
+//   });
+// }
